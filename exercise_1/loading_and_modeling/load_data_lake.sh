55f985a107412fb0213098d6306dc904d9f75239
w205 <w205@ip-172-31-20-105.ec2.internal>
2017-10-29 17:14:48 +0000

    final load_data_lake.sh

--- exercise_1/loading_and_modeling/load_data_lake.sh
+++ exercise_1/loading_and_modeling/load_data_lake.sh
@@ -24,11 +24,16 @@ tail -n +2 "hvbp_hcahps_05_28_2015.csv" > survey_responses.csv
 
 # Make Hadoop directories and move files to HDFS
 hdfs dfs -mkdir /user/w205/hospital_compare
-hdfs dfs -put hospitals.csv /user/w205/hospital_compare
-hdfs dfs -put effective_care.csv /user/w205/hospital_compare
-hdfs dfs -put readmissions.csv /user/w205/hospital_compare
-hdfs dfs -put measures.csv /user/w205/hospital_compare
-hdfs dfs -put survey_responses.csv /user/w205/hospital_compare
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/measures
+hdfs dfs -put measures.csv /user/w205/hospital_compare/measures
+hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
+hdfs dfs -put survey_responses.csv /user/w205/hospital_compare/survey_responses
 
 # Change back to current directory
 cd $my_dir
