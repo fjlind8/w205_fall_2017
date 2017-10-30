f181c596bd8c1d52ce8aba776eed7513a8e85994
fjlind8 <fjlind8@gmail.com>
2017-10-29 20:16:30 -0400

    Wrong table name

--- exercise_1/transforming/transforming_measure_dates.sql
+++
@@ -1,7 +0,0 @@
-drop table measure_dates_parquet;
-create table measure_dates_parquet as
-select
-measure_id,
-measure_name
-from measure_dates
-;
---
+++ exercise_1/transforming/transforming_measures.sql
@@ -0,0 +1,7 @@
+drop table measures_parquet;
+create table measures_parquet as
+select
+measure_id,
+measure_name
+from measures
+;
