a547a3ba6cdf11e509800e7bfbdbe42487ecc2ac
fjlind8 <fjlind8@gmail.com>
2017-10-30 23:39:39 -0400

    Update transforming_effective_care.sql

--- exercise_1/transforming/transforming_effective_care.sql
+++ exercise_1/transforming/transforming_effective_care.sql
@@ -5,9 +5,11 @@ provider_id,
 condition,
 measure_id,
 cast(score as int) score,
-cast(sample as int) sample
+cast(sample as int) sample,
+sum(cast(score as int)) * sum(cast(sample as int)) weighted_score
 from effective_care
 where measure_id <> 'EDV'
 and score <> 'Not Available'
 and footnote <> '11 - There were discrepancies in the data collection process.'
+group by provider_id, condition, measure_id, score, sample
 ;
