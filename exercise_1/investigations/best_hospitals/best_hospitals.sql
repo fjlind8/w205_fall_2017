230dd677020efa5826b84b687f108d01d44625e1
fjlind8 <fjlind8@gmail.com>
2017-10-31 03:36:16 -0400

    Correcting rounding

--- exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -1,7 +1,8 @@
-drop table best_states;
-create table best_states as
+drop table best_hospitals;
+create table best_hospitals as
 select
-variety.state,
+variety.provider_id,
+variety.hospital_name,
 0.4 * variety.weighted_avg_score + 0.6 * variety.weighted_avg_score as final_score,
 variety.weighted_avg_score variety_weighted_avg_score,
 severe.var_score variety_var_score,
@@ -10,11 +11,12 @@ severe.var_score severe_var_score
 from
 (
 select
-b.state,
+a.provider_id,
+b.hospital_name,
 count(measure_id) as measure_count,
 sum(sample) as population_count,
-round(cast(sum(score * sample) as float) / sum(sample), 4) weighted_avg_score,
-round(variance(score), 4) var_score
+round(cast(sum(score * sample) as float) / sum(sample), 4) as weighted_avg_score,
+round(variance(score), 4) as var_score
 from effective_care_parquet a
 join hospitals_parquet b on a.provider_id = b.provider_id
 where b.hospital_type <> 'Childrens'
@@ -31,14 +33,15 @@ and measure_id <> 'OP_5'
 and measure_id <> 'PC_01'
 and measure_id <> 'STK_4'
 and measure_id <> 'VTE_6'
-group by b.state
-having measure_count >= 30
+group by a.provider_id, b.hospital_name
+having measure_count >= 25
 ) variety
 
 join
 (
 select
-b.state,
+a.provider_id,
+b.hospital_name,
 count(measure_id) as measure_count,
 sum(denominator) as population_count,
 round(cast(sum(denominator * score) as float) / sum(denominator), 4) as weighted_avg_score,
@@ -47,9 +50,9 @@ from readmissions_parquet a
 join hospitals_parquet b on a.provider_id = b.provider_id
 where b.hospital_type <> 'Childrens'
 and b.hospital_type <> 'ACUTE CARE - VETERANS ADMINISTRATION'
-group by b.state
-having measure_count >= 10
-) severe on variety.state = severe.state
+group by a.provider_id, b.hospital_name
+having measure_count >= 7
+) severe on variety.provider_id = severe.provider_id
 order by final_score desc
 limit 10
 ;
