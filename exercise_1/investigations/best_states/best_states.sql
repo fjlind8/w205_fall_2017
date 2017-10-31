0f550a551a673617f2a1779a958f0876040c6bf8
fjlind8 <fjlind8@gmail.com>
2017-10-31 03:47:50 -0400

    Changed incorrect weighting logic to subtraction

--- exercise_1/investigations/best_states/best_states.sql
+++ exercise_1/investigations/best_states/best_states.sql
@@ -2,19 +2,19 @@ drop table best_states;
 create table best_states as
 select
 variety.state,
-0.4 * variety.weighted_avg_score + 0.6 * variety.weighted_avg_score as final_score,
-variety.weighted_avg_score variety_weighted_avg_score,
-severe.var_score variety_var_score,
-severe.weighted_avg_score severe_weighted_avg_score,
-severe.var_score severe_var_score
+round(variety.weighted_avg_score - severe.weighted_avg_score, 4) as final_score,
+round(variety.weighted_avg_score, 4) variety_weighted_avg_score,
+round(variety.var_score, 4) variety_var_score,
+round(severe.weighted_avg_score, 4) severe_weighted_avg_score,
+round(severe.var_score, 4) severe_var_score
 from
 (
 select
 b.state,
 count(measure_id) as measure_count,
 sum(sample) as population_count,
-round(cast(sum(score * sample) as float) / sum(sample), 4) weighted_avg_score,
-round(variance(score), 4) var_score
+cast(sum(score * sample) as float) / sum(sample) weighted_avg_score,
+variance(score) var_score
 from effective_care_parquet a
 join hospitals_parquet b on a.provider_id = b.provider_id
 where b.hospital_type <> 'Childrens'
@@ -41,8 +41,8 @@ select
 b.state,
 count(measure_id) as measure_count,
 sum(denominator) as population_count,
-round(cast(sum(denominator * score) as float) / sum(denominator), 4) as weighted_avg_score,
-round(variance(score), 4) as var_score
+cast(sum(denominator * score) as float) / sum(denominator) as weighted_avg_score,
+variance(score) as var_score
 from readmissions_parquet a
 join hospitals_parquet b on a.provider_id = b.provider_id
 where b.hospital_type <> 'Childrens'
