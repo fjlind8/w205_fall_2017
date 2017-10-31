0e8d8c412335e366c348ecd0744b634aab753297
fjlind8 <fjlind8@gmail.com>
2017-10-31 03:45:59 -0400

    Changed incorrect weighting to subtraction

--- exercise_1/investigations/best_hospitals/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals/best_hospitals.sql
@@ -3,9 +3,9 @@ create table best_hospitals as
 select
 variety.provider_id,
 variety.hospital_name,
-0.4 * variety.weighted_avg_score + 0.6 * variety.weighted_avg_score as final_score,
+round(variety.weighted_avg_score - severe.weighted_avg_score, 4) as final_score,
 variety.weighted_avg_score variety_weighted_avg_score,
-severe.var_score variety_var_score,
+variety.var_score variety_var_score,
 severe.weighted_avg_score severe_weighted_avg_score,
 severe.var_score severe_var_score
 from
@@ -15,8 +15,8 @@ a.provider_id,
 b.hospital_name,
 count(measure_id) as measure_count,
 sum(sample) as population_count,
-round(cast(sum(score * sample) as float) / sum(sample), 4) as weighted_avg_score,
-round(variance(score), 4) as var_score
+cast(sum(score * sample) as float) / sum(sample) as weighted_avg_score,
+variance(score) as var_score
 from effective_care_parquet a
 join hospitals_parquet b on a.provider_id = b.provider_id
 where b.hospital_type <> 'Childrens'
@@ -44,8 +44,8 @@ a.provider_id,
 b.hospital_name,
 count(measure_id) as measure_count,
 sum(denominator) as population_count,
-round(cast(sum(denominator * score) as float) / sum(denominator), 4) as weighted_avg_score,
-round(variance(score), 4) as var_score
+cast(sum(denominator * score) as float) / sum(denominator) as weighted_avg_score,
+variance(score) as var_score
 from readmissions_parquet a
 join hospitals_parquet b on a.provider_id = b.provider_id
 where b.hospital_type <> 'Childrens'
