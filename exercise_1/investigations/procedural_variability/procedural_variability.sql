81424c39fe3e0b00c55ee3dae61a05e55ed99b49
fjlind8 <fjlind8@gmail.com>
2017-10-31 04:24:20 -0400

    Update procedural_variability.sql

---
+++ exercise_1/investigations/procedural_variability/procedural_variability.sql
@@ -0,0 +1,29 @@
+create table procedural_variability as
+select
+measure_id,
+count(a.provider_id) provider_count,
+min(score) min_score,
+max(score) max_score,
+round(cast(sum(score * sample) as float) / sum(sample), 4) weighted_avg_score,
+round(sqrt(variance(score)), 4) standard_deviation_score
+from effective_care_parquet a
+join hospitals_parquet b on a.provider_id = b.provider_id
+where b.hospital_type <> 'Childrens'
+and b.hospital_type <> 'ACUTE CARE - VETERANS ADMINISTRATION'
+and measure_id <> 'ED_1b'
+and measure_id <> 'ED_2b'
+and measure_id <> 'OP_1'
+and measure_id <> 'OP_18b'
+and measure_id <> 'OP_20'
+and measure_id <> 'OP_21'
+and measure_id <> 'OP_22'
+and measure_id <> 'OP_3b'
+and measure_id <> 'OP_5'
+and measure_id <> 'PC_01'
+and measure_id <> 'STK_4'
+and measure_id <> 'VTE_6'
+group by measure_id
+having provider_count >= 1000
+order by standard_deviation_score desc
+limit 10
+;
