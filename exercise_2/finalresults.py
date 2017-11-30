69f9252f9959c7725c360069b6afa4ddf7d736f5
w205 <w205@ip-172-31-20-127.ec2.internal>
2017-11-30 01:30:40 +0000

    Updated finalresults.py

--- exercise_2/finalresults.py
+++ exercise_2/finalresults.py
@@ -1,15 +1,18 @@
 import sys
 import psycopg2
 
+# If there's more than one arguments entered, ask the user to resubmit with a single word, or no words
 if len(sys.argv) > 2:
     print '\nPlease enter just a single word, or no words!\n'
     exit(1)
 else:
     pass
 
+# Connect to the database tcount
 conn = psycopg2.connect(database = 'tcount', user = 'postgres', password = 'pass', host = 'localhost', port = '5432')
 cur  = conn.cursor()
 
+# Print the number of occurrences for a single word
 if len(sys.argv) == 2:
     word = sys.argv[1]
     
@@ -21,10 +24,12 @@ if len(sys.argv) == 2:
         print '\nTotal number of occurrences of "{}": {}\n'.format(word, word_count[0][1])
     else:
         print '\nTotal number of occurrences of "{}": 0\n'.format(word)
+# Print the number of occurrences for every word in the table, if there are records
 else:
     cur.execute('select * from tweetwordcount')
     
-    all_words = cur.fetchall()
+    # Sort on the word, case insensitive
+    all_words    = cur.fetchall()
     sorted_words = sorted(all_words, key = lambda x: x[0].lower())
 
     if len(sorted_words) > 0:
@@ -34,5 +39,6 @@ else:
     else:
         print '\nThere are no words in the database table tweetwordcount\n'
 
+# Close the connection
 conn.close()
 
