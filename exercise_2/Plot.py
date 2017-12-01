6954fab75884ad7d3d6f2be9e286d4fb1dd94196
w205 <w205@ip-172-31-20-127.ec2.internal>
2017-12-01 03:17:26 +0000

    Added plot

---
+++ exercise_2/Plot.py
@@ -0,0 +1,19 @@
+import psycopg2
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+conn = psycopg2.connect(database = 'tcount', user = 'postgres', password = 'pass', host = 'localhost', port = '5432')
+cur  = conn.cursor()
+
+cur.execute('select * from tweetwordcount order by count desc limit 20')
+
+counts    = cur.fetchall()
+df_counts = pd.DataFrame(counts, columns = ['word', 'count'])
+
+plt.bar(df_counts['word'], df_counts['count'], align = 'center')
+plt.ylabel('Word Count')
+plt.title('The Top 20 Words in My Twitter Stream')
+plt.savefig('Plot.png')
+
